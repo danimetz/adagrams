e5717f5a245d5b214f94d732fa7b660fe1b69283
Danielle Metzner <danimetzner@gmail.com>
2018-08-13 16:23:59 -0700

    created method to make sure anagrams use available letters in a players hand

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,54 +1,78 @@
 def draw_letters
-letters = {
-  "A" => 9,
-  "B" => 2,
-  "C" => 2,
-  "D" => 4,
-  "E" => 12,
-  "F" => 2,
-  "G" => 3,
-  "H" => 2,
-  "I" => 9,
-  "J" => 1,
-  "K" => 1,
-  "L" => 4,
-  "M" => 2,
-  "N" => 6,
-  "O" => 8,
-  "P" => 2,
-  "Q" => 1,
-  "R" => 6,
-  "S" => 4,
-  "T" => 6,
-  "U" => 4,
-  "V" => 2,
-  "W" => 2,
-  "X" => 1,
-  "Y" => 2,
-  "Z" => 1
-}
+  letters = {
+    "A" => 9,
+    "B" => 2,
+    "C" => 2,
+    "D" => 4,
+    "E" => 12,
+    "F" => 2,
+    "G" => 3,
+    "H" => 2,
+    "I" => 9,
+    "J" => 1,
+    "K" => 1,
+    "L" => 4,
+    "M" => 2,
+    "N" => 6,
+    "O" => 8,
+    "P" => 2,
+    "Q" => 1,
+    "R" => 6,
+    "S" => 4,
+    "T" => 6,
+    "U" => 4,
+    "V" => 2,
+    "W" => 2,
+    "X" => 1,
+    "Y" => 2,
+    "Z" => 1
+  }
 
-#Creates the array of all the letters available to pick
-letter_probability = []
-letters.each do |key,value|
-  until value == 0
-    letter_probability << key
-    value -= 1
+  #Creates the array of all the letters available to pick
+  letter_probability = []
+  letters.each do |key,value|
+    until value == 0
+      letter_probability << key
+      value -= 1
+    end
+  end
+
+  #Select letters and replace with 0 once they are pulled
+  your_hand = []
+  count = 0
+  while count < 10 do
+    index = rand(0...letter_probability.length)
+    if letter_probability[index] != 0
+      your_hand << letter_probability [index]
+      count += 1
+      letter_probability[index] = 0
+    end
+  end
+  return your_hand
+end
+
+def uses_available_letters(input, letters_in_hand)
+  input = input.split("")
+  check = []
+  if input.length > letters_in_hand.length
+    return false
+  else
+    input.each do |letter|
+      if input.count(letter) <= letters_in_hand.count(letter)
+        check << true
+      end
+    end
+  end
+  if input.length == check.length
+    return true
+  else
+    return false
   end
 end
-# puts letter_probability
 
+my_hand = draw_letters
 
-your_hand = []
-count = 0
-while count < 10 do
-  index = rand(0...letter_probability.length)
-  if letter_probability[index] != 0
-    your_hand << letter_probability [index]
-    count += 1
-  end
-end
-return your_hand
-end
+puts my_hand
+word = gets.chomp.upcase
 
-print draw_letters
+puts uses_available_letters(word, my_hand)
